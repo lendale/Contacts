b2509402bc51f953ae35a65a31c1667e85a55dc2
Allen Dale Gabisan <allen@usjr.edu.ph>
2017-12-08 23:54:01 +0800

    Initial commit without logic

--- app/src/main/java/com/gadiane/contacts/MainActivity.java
+++ app/src/main/java/com/gadiane/contacts/MainActivity.java
@@ -2,14 +2,21 @@ package com.gadiane.contacts;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    FloatingActionButton mFabAdd;
+    EditText mEtFirstName;
+    EditText mEtLastName;
+    EditText mEtContactNumber;
+    ListView mListContacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +25,16 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        initViews();
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
     @Override
@@ -48,5 +57,19 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    private void initViews() {
+        mEtFirstName = findViewById(R.id.etFirstName);
+        mEtLastName = findViewById(R.id.etLastName);
+        mEtContactNumber = findViewById(R.id.etContactNumber);
+        mListContacts = findViewById(R.id.lvContactsList);
+        mFabAdd = findViewById(R.id.fabAdd);
+        mFabAdd.setOnClickListener(MainActivity.this);
     }
 }
